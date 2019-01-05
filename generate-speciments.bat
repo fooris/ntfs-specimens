e196578f54079fdaff8ca91e4a0c5f368b26c4af
Joachim Metz <joachim.metz@gmail.com>
2019-01-05 10:05:59 +0100

    Added script to generate specimens

---
+++ generate-speciments.bat
@@ -0,0 +1,338 @@
+@echo off
+
+rem Script to generate NTFS test files
+rem Requires Windows 7 or later
+
+rem Split the output of ver e.g. "Microsoft Windows [Version 10.0.10586]"
+rem and keep the last part "10.0.10586]".
+for /f "tokens=1,2,3,4" %%a in ('ver') do (
+	set version=%%d
+)
+
+rem Replace dots by spaces "10 0 10586]".
+set version=%version:.= %
+
+rem Split the last part of the ver output "10 0 10586]" and keep the first
+rem 2 values formatted with a dot as separator "10.0".
+for /f "tokens=1,2,*" %%a in ("%version%") do (
+	set version=%%a.%%b
+)
+
+rem TODO add check for other supported versions of Windows
+rem Also see: https://en.wikipedia.org/wiki/Ver_(command)
+
+if not "%version%" == "10.0" (
+	echo Unsupported Windows version: %version%
+
+	exit /b 1
+)
+
+if exist "%version%" (
+	echo Specimens directory: %version% already exists.
+
+	exit /b 1
+)
+
+mkdir "%version%"
+
+rem Supported diskpart format fs=<FS> options: ntfs, fat, fat32
+rem Supported diskpart format unit=<N> options: 512, 1024, 2048, 4096 (default), 8192, 16K, 32K, 64K
+
+rem Create a fixed-size VHD image with a NTFS file system
+set unitsize=4096
+set imagename=ntfs.vhd
+set imagesize=4
+
+echo create vdisk file=%cd%\%version%\%imagename% maximum=%imagesize% type=fixed > CreateVHD.diskpart
+echo select vdisk file=%cd%\%version%\%imagename% >> CreateVHD.diskpart
+echo attach vdisk >> CreateVHD.diskpart
+echo convert mbr >> CreateVHD.diskpart
+echo create partition primary >> CreateVHD.diskpart
+
+echo format fs=ntfs label="TestVolume" unit=%unitsize% quick >> CreateVHD.diskpart
+
+echo assign letter=x >> CreateVHD.diskpart
+
+call :run_diskpart CreateVHD.diskpart
+
+call :create_test_file_entries x
+
+echo select vdisk file=%cd%\%version%\%imagename% > UnmountVHD.diskpart
+echo detach vdisk >> UnmountVHD.diskpart
+
+call :run_diskpart UnmountVHD.diskpart
+
+rem Create a fixed-size VHD image with a NTFS file system with compression
+set unitsize=4096
+set imagename=ntfs_compress.vhd
+set imagesize=4
+
+echo create vdisk file=%cd%\%version%\%imagename% maximum=%imagesize% type=fixed > CreateVHD.diskpart
+echo select vdisk file=%cd%\%version%\%imagename% >> CreateVHD.diskpart
+echo attach vdisk >> CreateVHD.diskpart
+echo convert mbr >> CreateVHD.diskpart
+echo create partition primary >> CreateVHD.diskpart
+
+echo format fs=ntfs label="TestVolume" unit=%unitsize% quick compress >> CreateVHD.diskpart
+
+echo assign letter=x >> CreateVHD.diskpart
+
+call :run_diskpart CreateVHD.diskpart
+
+call :create_test_file_entries x
+
+echo select vdisk file=%cd%\%version%\%imagename% > UnmountVHD.diskpart
+echo detach vdisk >> UnmountVHD.diskpart
+
+call :run_diskpart UnmountVHD.diskpart
+
+rem Create a fixed-size VHD image with a NTFS file system and unit size 512
+set unitsize=512
+set imagename=ntfs_%unitsize%.vhd
+set imagesize=4
+
+echo create vdisk file=%cd%\%version%\%imagename% maximum=%imagesize% type=fixed > CreateVHD.diskpart
+echo select vdisk file=%cd%\%version%\%imagename% >> CreateVHD.diskpart
+echo attach vdisk >> CreateVHD.diskpart
+echo convert mbr >> CreateVHD.diskpart
+echo create partition primary >> CreateVHD.diskpart
+
+echo format fs=ntfs label="TestVolume" unit=%unitsize% quick >> CreateVHD.diskpart
+
+echo assign letter=x >> CreateVHD.diskpart
+
+call :run_diskpart CreateVHD.diskpart
+
+call :create_test_file_entries x
+
+echo select vdisk file=%cd%\%version%\%imagename% > UnmountVHD.diskpart
+echo detach vdisk >> UnmountVHD.diskpart
+
+call :run_diskpart UnmountVHD.diskpart
+
+rem Create a fixed-size VHD image with a NTFS file system and unit size 1024
+set unitsize=1024
+set imagename=ntfs_%unitsize%.vhd
+set imagesize=4
+
+echo create vdisk file=%cd%\%version%\%imagename% maximum=%imagesize% type=fixed > CreateVHD.diskpart
+echo select vdisk file=%cd%\%version%\%imagename% >> CreateVHD.diskpart
+echo attach vdisk >> CreateVHD.diskpart
+echo convert mbr >> CreateVHD.diskpart
+echo create partition primary >> CreateVHD.diskpart
+
+echo format fs=ntfs label="TestVolume" unit=%unitsize% quick >> CreateVHD.diskpart
+
+echo assign letter=x >> CreateVHD.diskpart
+
+call :run_diskpart CreateVHD.diskpart
+
+call :create_test_file_entries x
+
+echo select vdisk file=%cd%\%version%\%imagename% > UnmountVHD.diskpart
+echo detach vdisk >> UnmountVHD.diskpart
+
+call :run_diskpart UnmountVHD.diskpart
+
+rem Create a fixed-size VHD image with a NTFS file system and unit size 2048
+set unitsize=2048
+set imagename=ntfs_%unitsize%.vhd
+set imagesize=4
+
+echo create vdisk file=%cd%\%version%\%imagename% maximum=%imagesize% type=fixed > CreateVHD.diskpart
+echo select vdisk file=%cd%\%version%\%imagename% >> CreateVHD.diskpart
+echo attach vdisk >> CreateVHD.diskpart
+echo convert mbr >> CreateVHD.diskpart
+echo create partition primary >> CreateVHD.diskpart
+
+echo format fs=ntfs label="TestVolume" unit=%unitsize% quick >> CreateVHD.diskpart
+
+echo assign letter=x >> CreateVHD.diskpart
+
+call :run_diskpart CreateVHD.diskpart
+
+call :create_test_file_entries x
+
+echo select vdisk file=%cd%\%version%\%imagename% > UnmountVHD.diskpart
+echo detach vdisk >> UnmountVHD.diskpart
+
+call :run_diskpart UnmountVHD.diskpart
+
+rem Create a fixed-size VHD image with a NTFS file system and unit size 4096
+set unitsize=4096
+set imagename=ntfs_%unitsize%.vhd
+set imagesize=4
+
+echo create vdisk file=%cd%\%version%\%imagename% maximum=%imagesize% type=fixed > CreateVHD.diskpart
+echo select vdisk file=%cd%\%version%\%imagename% >> CreateVHD.diskpart
+echo attach vdisk >> CreateVHD.diskpart
+echo convert mbr >> CreateVHD.diskpart
+echo create partition primary >> CreateVHD.diskpart
+
+echo format fs=ntfs label="TestVolume" unit=%unitsize% quick >> CreateVHD.diskpart
+
+echo assign letter=x >> CreateVHD.diskpart
+
+call :run_diskpart CreateVHD.diskpart
+
+call :create_test_file_entries x
+
+echo select vdisk file=%cd%\%version%\%imagename% > UnmountVHD.diskpart
+echo detach vdisk >> UnmountVHD.diskpart
+
+call :run_diskpart UnmountVHD.diskpart
+
+rem Create a fixed-size VHD image with a NTFS file system and unit size 8192
+set unitsize=8192
+set imagename=ntfs_%unitsize%.vhd
+set imagesize=4
+
+echo create vdisk file=%cd%\%version%\%imagename% maximum=%imagesize% type=fixed > CreateVHD.diskpart
+echo select vdisk file=%cd%\%version%\%imagename% >> CreateVHD.diskpart
+echo attach vdisk >> CreateVHD.diskpart
+echo convert mbr >> CreateVHD.diskpart
+echo create partition primary >> CreateVHD.diskpart
+
+echo format fs=ntfs label="TestVolume" unit=%unitsize% quick >> CreateVHD.diskpart
+
+echo assign letter=x >> CreateVHD.diskpart
+
+call :run_diskpart CreateVHD.diskpart
+
+call :create_test_file_entries x
+
+echo select vdisk file=%cd%\%version%\%imagename% > UnmountVHD.diskpart
+echo detach vdisk >> UnmountVHD.diskpart
+
+call :run_diskpart UnmountVHD.diskpart
+
+rem Create a fixed-size VHD image with a NTFS file system and unit size 16k
+set unitsize=16k
+set imagename=ntfs_%unitsize%.vhd
+set imagesize=4
+
+echo create vdisk file=%cd%\%version%\%imagename% maximum=%imagesize% type=fixed > CreateVHD.diskpart
+echo select vdisk file=%cd%\%version%\%imagename% >> CreateVHD.diskpart
+echo attach vdisk >> CreateVHD.diskpart
+echo convert mbr >> CreateVHD.diskpart
+echo create partition primary >> CreateVHD.diskpart
+
+echo format fs=ntfs label="TestVolume" unit=%unitsize% quick >> CreateVHD.diskpart
+
+echo assign letter=x >> CreateVHD.diskpart
+
+call :run_diskpart CreateVHD.diskpart
+
+call :create_test_file_entries x
+
+echo select vdisk file=%cd%\%version%\%imagename% > UnmountVHD.diskpart
+echo detach vdisk >> UnmountVHD.diskpart
+
+call :run_diskpart UnmountVHD.diskpart
+
+rem Create a fixed-size VHD image with a NTFS file system and unit size 32k
+set unitsize=32k
+set imagename=ntfs_%unitsize%.vhd
+set imagesize=4
+
+echo create vdisk file=%cd%\%version%\%imagename% maximum=%imagesize% type=fixed > CreateVHD.diskpart
+echo select vdisk file=%cd%\%version%\%imagename% >> CreateVHD.diskpart
+echo attach vdisk >> CreateVHD.diskpart
+echo convert mbr >> CreateVHD.diskpart
+echo create partition primary >> CreateVHD.diskpart
+
+echo format fs=ntfs label="TestVolume" unit=%unitsize% quick >> CreateVHD.diskpart
+
+echo assign letter=x >> CreateVHD.diskpart
+
+call :run_diskpart CreateVHD.diskpart
+
+call :create_test_file_entries x
+
+echo select vdisk file=%cd%\%version%\%imagename% > UnmountVHD.diskpart
+echo detach vdisk >> UnmountVHD.diskpart
+
+call :run_diskpart UnmountVHD.diskpart
+
+rem Create a fixed-size VHD image with a NTFS file system and unit size 64k
+set unitsize=64k
+set imagename=ntfs_%unitsize%.vhd
+set imagesize=4
+
+echo create vdisk file=%cd%\%version%\%imagename% maximum=%imagesize% type=fixed > CreateVHD.diskpart
+echo select vdisk file=%cd%\%version%\%imagename% >> CreateVHD.diskpart
+echo attach vdisk >> CreateVHD.diskpart
+echo convert mbr >> CreateVHD.diskpart
+echo create partition primary >> CreateVHD.diskpart
+
+echo format fs=ntfs label="TestVolume" unit=%unitsize% quick >> CreateVHD.diskpart
+
+echo assign letter=x >> CreateVHD.diskpart
+
+call :run_diskpart CreateVHD.diskpart
+
+call :create_test_file_entries x
+
+echo select vdisk file=%cd%\%version%\%imagename% > UnmountVHD.diskpart
+echo detach vdisk >> UnmountVHD.diskpart
+
+call :run_diskpart UnmountVHD.diskpart
+
+exit /b 0
+
+rem Creates test file entries
+:create_test_file_entries
+SETLOCAL
+SET driveletter=%1
+
+rem Create an emtpy file
+type nul >> %driveletter%:\emptyfile
+
+rem Create a directory
+mkdir %driveletter%:\testdir1
+
+rem Create a file
+echo My file > %driveletter%:\testdir1\testfile1
+
+rem Create a hard link to a file
+mklink /H %driveletter%:\file_hardlink1 %driveletter%:\testdir1\testfile1
+
+rem Create a symbolic link to a file
+mklink %driveletter%:\file_symboliclink1 %driveletter%:\testdir1\testfile1
+
+rem Create a junction (hard link to a directory)
+mklink /J %driveletter%:\directory_junction1 %driveletter%:\testdir1
+
+rem Create a symbolic link to a directory
+mklink /D %driveletter%:\directory_symboliclink1 %driveletter%:\testdir1
+
+rem Create a file with an altenative data stream (ADS)
+type nul >> %driveletter%:\ads1
+echo My ADS > %driveletter%:\ads1:myads
+
+ENDLOCAL
+exit /b 0
+
+rem Runs diskpart with a script
+rem Note that diskpart requires Administrator privileges to run
+:run_diskpart
+SETLOCAL
+set diskpartscript=%1
+
+rem Note that diskpart requires Administrator privileges to run
+diskpart /s %diskpartscript%
+
+if %errorlevel% neq 0 (
+	echo Failed to run: "diskpart /s %diskpartscript%"
+
+	exit /b 1
+)
+
+del /q %diskpartscript%
+
+rem Give the system a bit of time to adjust
+timeout /t 1 > nul
+
+ENDLOCAL
+exit /b 0
+
